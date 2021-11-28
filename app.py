cad8af0bfd547260b54c6b6cf309e5e466145140
Dawid Dęby <ddeby@totalbet.pl>
2021-11-28 14:35:35 +0100

    gitignore and config

--- app.py
+++ app.py
@@ -5,11 +5,43 @@ import os
 
 # Init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "db.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init Marshmallow
+ma = Marshmallow(app)
+
+# Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
 
 
-@app.route("/", methods=["GET"])
-def get():
-    return jsonify({})
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "description", "price", "qty")
+
+
+# Init Schema
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
 
 
 # Run Server
